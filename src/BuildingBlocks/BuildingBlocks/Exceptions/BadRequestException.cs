7c33614d450eead49f033b0ad66c47c5df0057b7
lehuuhieuak <01643815296h@gmail.com>
2025-06-29 17:07:38 +0700

    feat: add log, validate behavior and custom exception in buildingBlock

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildingBlocks.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+
+        public BadRequestException(string message, string detail) : base(message)
+        {
+            Details = detail;
+        }
+
+        public string? Details { get; }
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/InternalServerException.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildingBlocks.Exceptions
+{
+    public class InternalServerException : Exception
+    {
+        public InternalServerException(string message) : base(message)
+        {
+
+        }
+
+        public InternalServerException(string message, string detail) : base(message)
+        {
+            Details = detail;
+        }
+
+        public string? Details { get; }
+    }
+}
