1bc25a3587d1ccc3305dce56de762dc6f7bb59d7
lehuuhieuak <01643815296h@gmail.com>
2025-06-30 21:20:21 +0700

    refactor: rename file customExceptionHandler

--- src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 
 namespace BuildingBlocks.Exceptions.Handler
 {
-    public class CustomerExceptionHandler(ILogger<CustomerExceptionHandler> logger) : IExceptionHandler
+    public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
     {
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
