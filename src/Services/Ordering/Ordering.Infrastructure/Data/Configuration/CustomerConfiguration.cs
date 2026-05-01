7df764f5a3ac2858662f276f3e887e8adb72a2bf
lehuuhieuak <01643815296h@gmail.com>
2026-05-01 21:42:08 +0700

    feat: Ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configuration/CustomerConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ordering.Infrastructure.Data.Configuration
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasConversion(
+                customerId => customerId.Value,
+                dbId => CustomerId.Of(dbId));
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Email).IsRequired().HasMaxLength(255);
+            builder.HasIndex(c => c.Email).IsUnique();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ordering.Infrastructure.Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasConversion(
+                customerId => customerId.Value,
+                dbId => ProductId.Of(dbId));
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100).IsRequired();
+        }
+    }
+}
